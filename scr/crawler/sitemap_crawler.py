6f85444ade52d2d5838c8704852e1b2719c3c52c
Jonas <jonas.alker1@gmail.com>
2026-05-04 18:06:35 +0200

    Update sitemap_crawler.py: fixed typing errors Update requirements.txt: with dependencies from sitemap_crawler.py

--- scr/crawler/sitemap_crawler.py
+++ scr/crawler/sitemap_crawler.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin, urlpasrser, urlparse
+from urllib.parse import urljoin, urlparse
 from usp.tree import sitemap_tree_for_homepage
 
 def search_sitemap_by_url(portal_name, portal_url):
     """
     :param portal_name: 
-    :param (portal_url: 
+    :param portal_url:
     :return: 
     """
     urls = set()
@@ -36,7 +36,7 @@ def search_sitemap_by_url(portal_name, portal_url):
 def manual_crawl(url):
     urls = set()
     to_visit = [url]
-    domain = urlpasrser(url).netloc
+    domain = urlparse(url).netloc
 
     while to_visit:
         current_url = to_visit.pop(0)
@@ -48,7 +48,7 @@ def manual_crawl(url):
             soup = BeautifulSoup(r.text, "html.parser")
             for link in soup.find_all("a", href=True):
                 full_url = urljoin(current_url, link["href"]).split('#')[0].rstrip('/')
-                if urlparse(full_urls).netloc == domain and full_url not in urls:
+                if urlparse(full_url).netloc == domain and full_url not in urls:
                     to_visit.append(full_url)
         except Exception as e:
             print(f"Sitemap Manual Crawl Error: {e}")
