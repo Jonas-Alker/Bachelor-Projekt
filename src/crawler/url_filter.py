1400951aa812ef12a656ff9f0c052d94f20ea465
Jonas <jonas.alker1@gmail.com>
2026-05-18 15:15:37 +0200

    refactor: change URL filtering in `url_filter.py` from list-based to atomic function

--- src/crawler/url_filter.py
+++ src/crawler/url_filter.py
@@ -1,7 +1,6 @@
 import json
 import os
 
-
 def load_rules(portal_name, config_path="config/filter_rules.json"):
     """Loads inclusion and exclusion rules for a specific portal from a JSON config file.
     If the specified portal configuration does not exist, it falls back to a 'default' rule set.
@@ -11,9 +10,7 @@ def load_rules(portal_name, config_path="config/filter_rules.json"):
     :param portal_name: The name of the specific web portal.
     :param config_path: The file path to the JSON configuration file.
     :return: A tuple containing two lists of strings: (include_rules, exclude_rules).
-
     """
-
     if not(os.path.exists(config_path)):
         return ["artikel"],["impressum"]
 
@@ -23,30 +20,17 @@ def load_rules(portal_name, config_path="config/filter_rules.json"):
     portal_rules = rules.get(portal_name, rules.get("default"))
     return portal_rules["include"], portal_rules["exclude"]
 
-def filter_urls(portal_name, input_base="data/raw", output_base="data/filtered"):
-    include, exclude = load_rules(portal_name)
+def filter_url(url, include,exclude):
+    """Determines whether a given URL should be kept based on include and exclude rules
 
-    input_file = (f"{input_base}/{portal_name}_urls.txt")
-    if not os.path.exists(input_file):
-        print(f"Input file {input_file} does not exist")
-        return
-
-    output_dir = output_base
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    output_file = (f"{output_dir}/{portal_name}_filtered_urls.txt")
-
-    relevant_urls = []
-    with open(input_file, "r", encoding="utf-8") as f:
-        for line in f:
-            url = line.strip().lower()
-
-            if any(word in url for word in exclude):
-                continue
-
-            if any(word in url for word in include):
-                relevant_urls.append(url)
-
-    with open(output_file, "w", encoding="utf-8") as f:
-        for url in relevant_urls:
-            f.write(f"{url}\n")
+    :param url: The URL string to evaluate.
+    :param include: A list of keywords that a URL must contain to be considered valid.
+    :param exclude: A list of keywords that will cause a URL to be rejected immediately.
+    :return: True if the URL passes the filtering criteria; False otherwise
+    """
+    if any(word in url for word in exclude):
+        return False
+    elif any(word in url for word in include):
+        return True
+    else:
+        return False
