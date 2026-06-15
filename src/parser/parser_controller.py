c89dbe522a37719bb5998a73aeaa7904cf38e307
Jonas <jonas.alker1@gmail.com>
2026-06-15 11:59:20 +0200

    add: pass-through functions in 'preprocessor_controller.py' and 'parser_controller.py'
    - pass-through functions determine which module to pass the method call to

--- src/parser/parser_controller.py
+++ src/parser/parser_controller.py
@@ -1,6 +1,8 @@
+import src.parser.llm_parser as llm_parser
 import os
+import sys
 from pathlib import Path
-
+import importlib.util
 
 GENERATED_DIR = Path(__file__).resolve().parent / "generated"
 
@@ -20,3 +22,29 @@ def get_existing_parsers():
             parsers.append(portal_name)
 
     return parsers
+
+
+def parse(portal_name, html,llm_based=False):
+    """
+
+    :param portal_name:
+    :param html:
+    :return:
+    """
+    if llm_based:
+        return llm_parser.parse_factcheck(html)
+    else:
+        file_path = GENERATED_DIR / f"{portal_name.lower()}_preprocessor.py"
+        if not file_path.exists():
+            raise FileNotFoundError(f"Preprocessor for {portal_name.lower()} does not exist")
+
+        module_name = f"{portal_name.lower()}_preprocessor"
+        spec = importlib.util.spec_from_file_location(module_name, file_path)
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[module_name] = module
+        spec.loader.exec_module(module)
+
+        if hasattr(module, 'preprocess_faktencheck'):
+            return module.preprocess_faktencheck(html)
+        else:
+            raise AttributeError(f"Preprocessor for {portal_name.lower()} has no function preprocess_faktencheck.")
--- src/preprocessor/preprocessor_controller.py
+++ src/preprocessor/preprocessor_controller.py
@@ -1,5 +1,7 @@
 import os
+import sys
 from pathlib import Path
+import importlib.util
 
 GENERATED_DIR = Path(__file__).resolve().parent / "generated"
 
@@ -19,3 +21,19 @@ def get_existing_preprocessors():
             preprocessors.append(portal_name)
 
     return preprocessors
+
+def preprocess(portal_name, html):
+    file_path = GENERATED_DIR / f"{portal_name.lower()}_preprocessor.py"
+    if not file_path.exists():
+        raise FileNotFoundError(f"Preprocessor for {portal_name.lower()} does not exist")
+
+    module_name = f"{portal_name.lower()}_preprocessor"
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+
+    if hasattr(module, 'preprocess_faktencheck'):
+        return module.preprocess_faktencheck(html)
+    else:
+        raise AttributeError(f"Preprocessor for {portal_name.lower()} has no function preprocess_faktencheck.")
