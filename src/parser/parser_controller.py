3c8f73458781f8de40130f6f69b465c634d4bc43
Jonas <jonas.alker1@gmail.com>
2026-06-15 23:21:29 +0200

    fix: checking for right method and path in 'preprocessor_controller.py'
    - changed check for existing 'preprocess_faktencheck' method in loaded modul to 'parse_factcheck'
    - changed loaded module and file path from  ..._preprocessor to ..._parser

--- src/parser/parser_controller.py
+++ src/parser/parser_controller.py
@@ -34,17 +34,17 @@ def parse(portal_name, html,llm_based=False):
     if llm_based:
         return llm_parser.parse_factcheck(html)
     else:
-        file_path = GENERATED_DIR / f"{portal_name.lower()}_preprocessor.py"
+        file_path = GENERATED_DIR / f"{portal_name.lower()}_parser.py"
         if not file_path.exists():
-            raise FileNotFoundError(f"Preprocessor for {portal_name.lower()} does not exist")
+            raise FileNotFoundError(f"Parser for {portal_name.lower()} does not exist")
 
-        module_name = f"{portal_name.lower()}_preprocessor"
+        module_name = f"{portal_name.lower()}_parser"
         spec = importlib.util.spec_from_file_location(module_name, file_path)
         module = importlib.util.module_from_spec(spec)
         sys.modules[module_name] = module
         spec.loader.exec_module(module)
 
-        if hasattr(module, 'preprocess_faktencheck'):
-            return module.preprocess_faktencheck(html)
+        if hasattr(module, 'parse_factcheck'):
+            return module.parse_factcheck(html)
         else:
-            raise AttributeError(f"Preprocessor for {portal_name.lower()} has no function preprocess_faktencheck.")
+            raise AttributeError(f"Preprocessor for {portal_name.lower()} has no function parse_factcheck.")
