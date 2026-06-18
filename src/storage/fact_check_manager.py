42da5bf111562336e67764098092e9a48013d691
Jonas <jonas.alker1@gmail.com>
2026-06-18 12:40:08 +0200

    refactor: enhance 'FactCheckManager' with JSON repair, table reset, and CSV export
    - Update '_setup_db' to drop all existing tables prior to initialization for a clean database state
    - Expand 'add_fact_check' to automatically repair faulty JSON inputs and correct invalid SQL fields
    - Implement 'export_as_csv' method to enable database exports to CSV format

--- src/storage/fact_check_manager.py
+++ src/storage/fact_check_manager.py
@@ -1,10 +1,13 @@
+import json
 import sqlite3
 import os
 import shutil
+import pandas as pd
 
 class FactCheckManager:
     def __init__(self, version= "v1", mode="create", base_path="data/", source_path = None):
-        """Initializes the DBManager and establishes the database file path.
+        """
+        Initializes the FactCheckManager and establishes the database file path.
 
         :param version: The version string of the database (used in the filename).
         :param mode: Operation mode; "create" initializes a new DB, "load" expects an existing one
@@ -12,9 +15,9 @@ class FactCheckManager:
         :param base_path: The directory path where the database file will be stored.
 
         Raises:
-            FileNotFoundError: If mode is "load" but the database file does not exist,
-                or if mode is "copy" but the source_path file does not exist.
-            ValueError: If mode is "copy" but no source_path is provided.
+            FileNotFoundError:  If mode is "load" but the database file does not exist,
+                                or if mode is "copy" but the source_path file does not exist.
+            ValueError:         If mode is "copy" but no source_path is provided.
         """
         self.db_path = os.path.join(base_path, f"factencheck_{version}.db")
 
@@ -37,7 +40,8 @@ class FactCheckManager:
             self._setup_db()
 
     def _get_connection(self):
-        """Creates and returns a connection to the SQLite database.
+        """
+        Creates and returns a connection to the SQLite database.
         Configures the row factory to `sqlite3.Row` to allow fetching rows
         as dictionary-like objects accessible by column names.
 
@@ -49,13 +53,22 @@ class FactCheckManager:
         return conn
 
     def _setup_db(self):
-        """Initializes the database schema by creating the required tables.
+        """
+        Initializes the database schema by creating the required tables.
 
         Warning:
-            This method drops the existing 'web_cache' table and clears all
-            previously stored data if it already exists.
+            This method drops all the existing tables and clears all
+            previously stored data.
         """
         with self._get_connection() as conn:
+            # 0. Dropping Tables
+            conn.execute("PRAGMA foreign_keys = OFF")
+            conn.execute("DROP TABLE IF EXISTS portals")
+            conn.execute("DROP TABLE IF EXISTS claim_reviews")
+            conn.execute("DROP TABLE IF EXISTS claims")
+            conn.execute("DROP TABLE IF EXISTS claim_ratings")
+            conn.execute("PRAGMA foreign_keys = ON")
+
             # 1. Portal
             conn.execute("""
                 CREATE TABLE IF NOT EXISTS portals
@@ -106,31 +119,66 @@ class FactCheckManager:
 
     def add_fact_check(self, portal_name, portal_url, factcheck_url, claims_data):
         """
-        :param
+        Adds a fact check to the database. First, it checks whether `claims_data` is a valid JSON object.
+        If this is not the case, it attempts to repair it; otherwise, a warning is issued and the process is skipped.
+
+        :param portal_name: name of the portal
+        :param portal_url: url of the portal
+        :param factcheck_url: url of the fact check
+        :param claims_data: claims data, which contains all the extracted data and conforms to the defined JSON format
         """
+        if isinstance(claims_data, str):
+            claims_data = json.loads(claims_data)
+
+        # Case A: The LLM has accidentally created nested lists: [[{...}]]
+        if isinstance(claims_data, list) and len(claims_data) > 0 and isinstance(claims_data[0], list):
+            claims_data = claims_data[0]
+
+        # Case B: The LLM has provided just a single dictionary instead of a list: {...}
+        elif isinstance(claims_data, dict):
+            claims_data = [claims_data]
+
+        # Case C: Handling empty data or a completely corrupted format
+        if not claims_data or not isinstance(claims_data, list):
+            print(f"Warning: Invalid data format for {factcheck_url}. Skipping entry.")
+            return
+
         with self._get_connection() as conn:
+
             #Portal
             conn.execute("INSERT OR IGNORE INTO portals (name, portal_url) VALUES (?, ?)", (portal_name,portal_url))
-            portal_id = conn.execute("SELECT id FROM sources WHERE name = ?", (portal_name,)).fetchone()[0]
+            portal_id = conn.execute("SELECT id FROM portals WHERE name = ?", (portal_name,)).fetchone()[0]
 
-            #Because of possible multiple claims in claims_data
-            for claim in claims_data:
+            for claim in claims_data:  #Loop due to possible multiple entries in ‘claims_data’
 
                 #Review
-                conn.execute("INSERT OR IGNORE INTO claim_reviews (portal_id, headline, body, author, published, url) VALUES (?, ?, ?, ?, ?,?)",
-                             (portal_id, claim["headline"], claim["body"], claim["author"],
-                                        claim["published_at"],factcheck_url))
+                conn.execute("INSERT OR IGNORE INTO claim_reviews (portal_id, headline, body, author, published, url ,language) "
+                             "VALUES (?, ?, ?, ?, ?, ?, ?)",
+                             (portal_id, claim["headline"], claim["body"], claim["author_factcheck"],
+                                        claim["published_at"],factcheck_url, claim["language"]))
                 review_id = conn.execute("SELECT id FROM claim_reviews WHERE url = ?", (factcheck_url,)).fetchone()[0]
 
                 #Claim
                 conn.execute(
                     "INSERT OR IGNORE INTO claims (claim, author, stated_at) VALUES (?, ?, ?)",
-                    (claim["claim"], claim["claim_author"], claim["stated_at"]))
-                claim_id = conn.execute("SELECT id FROM claims WHERE claim = ? AND author = ?",
-                                        (claim["claim"], claim["claim_author"])).fetchone()[0]
+                    (claim["claim"], claim["author_claim"], claim["stated_at"]))
+                claim_id = conn.execute("SELECT id FROM claims WHERE claim IS ? AND author IS ?",
+                                        (claim["claim"], claim["author_claim"])).fetchone()[0]
 
                 #claim_ratings
-                conn.execute("""INSERT INTO claim_ratings (claim_review_id, claim_id, rating_original)
+                conn.execute("""INSERT OR IGNORE INTO  claim_ratings (claim_review_id, claim_id, rating_original)
                                     VALUES (?, ?, ?)""",
-                                 (review_id, claim_id, claim["rating"]))
+                                 (review_id, claim_id, claim["original_rating"]))
                 conn.commit()
+
+    def export_as_csv(self,path):
+        """
+        Exports the joined tables from the database as a CSV file
+
+        :param path: path where the CSV file will be saved
+        """
+        with self._get_connection() as conn:
+            df = pd.read_sql_query("""SELECT * FROM portals JOIN claim_reviews ON portals.id = claim_reviews.portal_id JOIN
+                                    claim_ratings ON claim_ratings.claim_review_id = claim_reviews.id  JOIN
+                                    claims ON claims.id = claim_ratings.claim_id """, conn)
+            df.to_csv(path, index=False)
