f1b836edb100a53c59bbb5c4a7845a159166545b
Jonas <jonas.alker1@gmail.com>
2026-08-07 14:41:49 +0200

    refactor: clean up 'main_evaluation.py' and add crawler test
    - Remove obsolete testing functions and logic from 'main_evaluation.py'
    - Move the removed code to the 'deprecated' directory to keep the main evaluation pipeline clean
    - Add a new dedicated test for the crawling pipeline to ensure proper functionality

--- main_evaluation.py
+++ main_evaluation.py
@@ -1,14 +1,12 @@
 import argparse
 
 from evaluation.evaluation_preprocess_and_parse import (
-    test_llm_directly_extraction_quality_against_claims_kg,
     test_llm_directly_extraction_quality_against_ground_truth,
-    test_llm_preprocessed_extraction_quality_against_claims_kg,
     test_llm_preprocessed_extraction_quality_against_ground_truth,
     test_claims_kg_quality_against_ground_truth,
-    test_parser_against_claims_kg,
     test_parser_against_ground_truth
 )
+from evaluation.evaluation_crawl import test_ClaimsKG_comparision
 import logging.config
 from logging_config import LOGGING_SETUP
 
@@ -22,40 +20,32 @@ def main():
         description="Management of the fact-checking evaluation pipeline."
     )
     parser.add_argument(
-        '--run-llm-directly-claims-kg', action='store_true',
-        help="Runs test_llm_directly_extraction_quality_against_claims_kg"
-    )
-    parser.add_argument(
         '--run-llm-directly-ground-truth', action='store_true',
-        help="Runs test_llm_directly_extraction_quality_against_ground_truth"
-    )
-    parser.add_argument(
-        '--run-llm-preprocessed-claims-kg', action='store_true',
-        help="Runs test_llm_preprocessed_extraction_quality_against_claims_kg"
+        help="Runs test_llm_directly_extraction_quality_against_ground_truth."
     )
     parser.add_argument(
         '--run-llm-preprocessed-ground-truth', action='store_true',
-        help="Runs test_llm_preprocessed_extraction_quality_against_ground_truth"
+        help="Runs test_llm_preprocessed_extraction_quality_against_ground_truth."
     )
     parser.add_argument(
         '--run-claims-kg-ground-truth', action='store_true',
-        help="Runs test_claims_kg_quality_against_ground_truth"
-    )
-    parser.add_argument(
-        '--run-parser-claims-kg', action='store_true',
-        help="Runs test_parser_against_claims_kg"
+        help="Runs test_claims_kg_quality_against_ground_truth."
     )
     parser.add_argument(
         '--run-parser-ground-truth', action='store_true',
-        help="Runs test_parser_against_ground_truth"
+        help="Runs test_parser_against_ground_truth."
     )
-
-
-
     parser.add_argument(
-        '--run-all',
-        action='store_true',
+        '--run-crawl-evaluation', action='store_true',
+        help="Runs the crawl evaluation for politifact. WARNING: May take a very long time."
+    )
+    parser.add_argument(
+        '--run-all', action='store_true',
         help="Runs all available evaluations one after the other."
+    )
+    parser.add_argument(
+        '--run-all-extraction', action='store_true',
+        help="Runs all extraction evaluations one after the other (excluding run-crawl-evaluation)."
     )
     parser.add_argument(
         '--language', type=str, default="english",
@@ -65,10 +55,13 @@ def main():
     args = parser.parse_args()
 
     run_flags = [
-        args.run_llm_directly_claims_kg, args.run_llm_directly_ground_truth,
-        args.run_llm_preprocessed_claims_kg, args.run_llm_preprocessed_ground_truth,
-        args.run_claims_kg_ground_truth, args.run_parser_claims_kg,
-        args.run_parser_ground_truth, args.run_all
+        args.run_llm_directly_ground_truth,
+        args.run_llm_preprocessed_ground_truth,
+        args.run_claims_kg_ground_truth,
+        args.run_parser_ground_truth,
+        args.run_crawl_evaluation,
+        args.run_all,
+        args.run_all_extraction
     ]
 
     if not any(run_flags):
@@ -85,35 +78,27 @@ def main():
     logger.info("Start evaluation pipeline...")
 
     try:
-        if args.run_all or args.run_llm_directly_claims_kg:
-            logger.info("Running: test_llm_directly_extraction_quality_against_claims_kg...")
-            test_llm_directly_extraction_quality_against_claims_kg()
-
-        if args.run_all or args.run_llm_directly_ground_truth:
+        if args.run_all or args.run_all_extraction or args.run_llm_directly_ground_truth:
             logger.info(
                 f"Running: test_llm_directly_extraction_quality_against_ground_truth (language: {args.language})...")
             test_llm_directly_extraction_quality_against_ground_truth(language=args.language)
 
-        if args.run_all or args.run_llm_preprocessed_claims_kg:
-            logger.info("Running: test_llm_preprocessed_extraction_quality_against_claims_kg...")
-            test_llm_preprocessed_extraction_quality_against_claims_kg()
-
-        if args.run_all or args.run_llm_preprocessed_ground_truth:
+        if args.run_all or args.run_all_extraction  or args.run_llm_preprocessed_ground_truth:
             logger.info(
                 f"Running: test_llm_preprocessed_extraction_quality_against_ground_truth (language: {args.language})...")
             test_llm_preprocessed_extraction_quality_against_ground_truth(language=args.language)
 
-        if args.run_all or args.run_claims_kg_ground_truth:
+        if args.run_all or args.run_all_extraction  or args.run_claims_kg_ground_truth:
             logger.info("Running: test_claims_kg_quality_against_ground_truth...")
             test_claims_kg_quality_against_ground_truth()
 
-        if args.run_all or args.run_parser_claims_kg:
-            logger.info("Running: test_parser_against_claims_kg...")
-            test_parser_against_claims_kg()
-
-        if args.run_all or args.run_parser_ground_truth:
+        if args.run_all or args.run_all_extraction  or args.run_parser_ground_truth:
             logger.info(f"Running: test_parser_against_ground_truth (language: {args.language})...")
             test_parser_against_ground_truth(language=args.language)
+
+        if args.run_all or args.run_crawl_evaluation:
+            logger.info("Running: crawl_evaluation...")
+            test_ClaimsKG_comparision()
 
     except Exception as e:
         logger.error(f"Critical error during evaluation: {e}")
